4375225ea937aa60cc1ffa6cb72a1ee498c5f2d9
Ahmad A <25608913+damywise@users.noreply.github.com>
2024-04-14 13:44:26 +0700

    feat: more shapes and path example

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,15 +1,20 @@
 use anyhow::anyhow;
 use anyhow::bail;
 use anyhow::Ok;
-use vello::block_on_wgpu;
 use core::num::NonZeroUsize;
+use vello::block_on_wgpu;
 use vello::kurbo::Affine;
 use vello::kurbo::Circle;
+use vello::kurbo::Ellipse;
+use vello::kurbo::Line;
+use vello::kurbo::RoundedRect;
+use vello::kurbo::Stroke;
 use vello::peniko::Color;
 use vello::util::RenderContext;
 use vello::AaConfig;
 use vello::Renderer;
 use vello::RendererOptions;
+use vello::Scene;
 use wgpu::BufferDescriptor;
 use wgpu::BufferUsages;
 use wgpu::CommandEncoderDescriptor;
@@ -67,13 +72,14 @@ async fn render() -> Result<Vec<u8>, anyhow::Error> {
 
     // Create scene and draw stuff in it
     let mut scene = vello::Scene::new();
-    scene.fill(
-        vello::peniko::Fill::NonZero,
-        Affine::IDENTITY,
-        Color::rgb8(242, 140, 168),
-        None,
-        &Circle::new((420.0, 200.0), 120.0),
-    );
+    // scene.fill(
+    //     vello::peniko::Fill::NonZero,
+    //     Affine::IDENTITY,
+    //     Color::rgb8(242, 140, 168),
+    //     None,
+    //     &Circle::new((420.0, 200.0), 120.0),
+    // );
+    add_shapes_to_scene(&mut scene);
 
     let target = device.create_texture(&TextureDescriptor {
         label: Some("Target texture"),
@@ -158,4 +164,68 @@ async fn render() -> Result<Vec<u8>, anyhow::Error> {
     }
 
     Ok(result_unpadded)
+}
+
+fn add_shapes_to_scene(scene: &mut Scene) {
+    // Draw an outlined rectangle
+    let stroke = Stroke::new(6.0);
+    let rect = RoundedRect::new(10.0, 10.0, 240.0, 240.0, 20.0);
+    let rect_stroke_color = Color::rgb(0.9804, 0.702, 0.5294);
+    scene.stroke(&stroke, Affine::IDENTITY, rect_stroke_color, None, &rect);
+
+    // Draw a filled circle
+    let circle = Circle::new((420.0, 200.0), 120.0);
+    let circle_fill_color = Color::rgb(0.9529, 0.5451, 0.6588);
+    scene.fill(
+        vello::peniko::Fill::NonZero,
+        Affine::IDENTITY,
+        circle_fill_color,
+        None,
+        &circle,
+    );
+
+    // Draw a filled ellipse
+    let ellipse = Ellipse::new((250.0, 420.0), (100.0, 160.0), -90.0);
+    let ellipse_fill_color = Color::rgb(0.7961, 0.651, 0.9686);
+    scene.fill(
+        vello::peniko::Fill::NonZero,
+        Affine::IDENTITY,
+        ellipse_fill_color,
+        None,
+        &ellipse,
+    );
+
+    // Draw a straight line
+    let line = Line::new((260.0, 20.0), (620.0, 100.0));
+    let line_stroke_color = Color::rgb(0.5373, 0.7059, 0.9804);
+    scene.stroke(&stroke, Affine::IDENTITY, line_stroke_color, None, &line);
+
+    // Draw a Bezier curve
+    let bezier = vello::kurbo::BezPath::from_vec(vec![
+        vello::kurbo::PathEl::MoveTo((200.0, 300.0).into()),
+        vello::kurbo::PathEl::CurveTo(
+            (250.0, 350.0).into(),
+            (350.0, 250.0).into(),
+            (400.0, 300.0).into(),
+        ),
+        vello::kurbo::PathEl::ClosePath,
+    ]);
+    let bezier_stroke_color = Color::rgb(0.4824, 0.7882, 0.4353);
+    scene.stroke(
+        &stroke,
+        Affine::IDENTITY,
+        bezier_stroke_color,
+        None,
+        &bezier,
+    );
+
+    // Draw a path
+    let mut path = vello::kurbo::BezPath::new();
+    path.move_to((100.0, 100.0));
+    path.line_to((150.0, 150.0));
+    path.quad_to((200.0, 100.0), (250.0, 150.0));
+    path.curve_to((300.0, 200.0), (350.0, 150.0), (400.0, 200.0));
+    path.close_path();
+    let path_stroke_color = Color::rgb(0.8353, 0.2392, 0.3098);
+    scene.stroke(&stroke, Affine::IDENTITY, path_stroke_color, None, &path);
 }
